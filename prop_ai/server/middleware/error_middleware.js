634b78b61609e3f629a9418c6b42fcde8b81d00f
Dany Ashraf <danyashraf12@gmail.com>
2026-09-14 19:58:16 +0300

    fix bug

--- prop_ai/server/middleware/error_middleware.js
+++ prop_ai/server/middleware/error_middleware.js
@@ -1,27 +1,8 @@
-const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config({ path: "config/.env" });
-const morgan = require("morgan");
-const { connectDB } = require("./config/db");
-const { ApiError } = require("./utils/api_error");
-const { globalErrorHandler } = require("./middleware/error_middleware");
-const app = express();
-
-if(process.env.NODE_ENV === "development"){
-    app.use(morgan("dev"));
+exports.globalErrorHandler = (err,req,res,next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    });
 }
-connectDB();
-
-
-app.all('',(req,res,next)=>{
-  const error = new ApiError(`Can't find ${req.originalUrl} on this server!`,404);
-  next(error);
-})
-
-app.use(globalErrorHandler );
-
-
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
-});
